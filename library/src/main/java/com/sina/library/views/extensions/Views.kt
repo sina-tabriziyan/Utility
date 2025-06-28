4697b4264a71e1e1cc52235d9e8b5dadddadf2b6
sina <sina.tabriziyan@gmail.com>
2025-06-28 16:28:57 +0330

    v0.1.6

--- library/src/main/java/com/sina/library/views/extensions/Views.kt
+++ library/src/main/java/com/sina/library/views/extensions/Views.kt
@@ -1035,6 +1035,7 @@ object ViewExtensions {
                             if (isMail) R.drawable.no_user_male else R.drawable.no_user_female
                         )
                     )
+                    crossfade(200)
                 }
                 .transformations(CircleCropTransformation())
                 .target(this)
