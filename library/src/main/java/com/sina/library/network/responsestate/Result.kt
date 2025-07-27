abf914cd1a8ecc4ad2cb07b4b73e7d64d32b0eb0
sina <sina.tabriziyan@gmail.com>
2025-07-27 10:56:29 +0330

    v0.2.27

--- library/src/main/java/com/sina/library/network/responsestate/Result.kt
+++ library/src/main/java/com/sina/library/network/responsestate/Result.kt
@@ -1,5 +1,8 @@
 package com.sina.library.network.responsestate
 
+import retrofit2.Response
+import java.io.IOException
+
 
 sealed interface Result<out D, out E : RootError> {
     data class Success<out D, out E : RootError>(val data: D) : Result<D, E>
@@ -37,3 +40,29 @@ inline fun <D, E : RootError, R> Result<D, E>.map(transform: (D) -> R): Result<R
         is Result.Error -> Result.Error(error)
     }
 
+inline fun <reified T, E : RootError> Result<Unit, E>.mapTo(data: T): Result<T, E> {
+    return when (this) {
+        is Result.Success -> Result.Success(data)
+        is Result.Error -> Result.Error(error)
+    }
+}
+
+suspend inline fun <reified T> safeCall(
+    crossinline apiCall: suspend () -> Response<T>
+): Result<T, DataError.Network> {
+    return try {
+        val response = apiCall()
+        val body = response.body()
+        if (response.isSuccessful && body != null) {
+            Result.Success(body)
+        } else
+            Result.Error(DataError.Network.UNKNOWN)
+    } catch (e: IOException) {
+        Result.Error(DataError.Network.NO_INTERNET)
+    }
+}
+
+inline fun <T, E : RootError> Result<ApiSuccess<T>, E>.asResultBody(): Result<T, E> = when (this) {
+    is Result.Error -> Result.Error(this.error)
+    is Result.Success -> Result.Success(this.data.body)
+}
--- library/src/main/java/com/sina/library/network/responsestate/safeApiCall.kt
+++ library/src/main/java/com/sina/library/network/responsestate/safeApiCall.kt
@@ -40,9 +40,38 @@ suspend inline fun <reified T> safeApiCall(
 }
 
 
-inline fun <reified T, E : RootError> Result<Unit, E>.mapTo(data: T): Result<T, E> {
-    return when (this) {
-        is Result.Success -> Result.Success(data)
-        is Result.Error -> Result.Error(error)
+suspend inline fun <reified T> safeApiCall(
+    crossinline apiCall: suspend () -> Response<T>,
+    successCodes: List<Int> = listOf(200), // default: only 200
+    treatEmptyBodyAsError: Boolean = true  // config for nullable body
+): Result<ApiSuccess<T>, DataError.Network> {
+    return try {
+        val response = apiCall()
+        val code = response.code()
+        val body = response.body()
+
+        if (code in successCodes) {
+            if (body != null || !treatEmptyBodyAsError) {
+                Result.Success(ApiSuccess(code, body as T)) // safe cast: body could be null if allowed
+            } else {
+                Result.Error(DataError.Network.SERIALIZATION)
+            }
+        } else {
+            val error = when (code) {
+                408 -> DataError.Network.REQUEST_TIMEOUT
+                429 -> DataError.Network.TOO_MANY_REQUESTS
+                413 -> DataError.Network.PAYLOAD_TOO_LARGE
+                in 500..599 -> DataError.Network.SERVER_ERROR
+                else -> DataError.Network.UNKNOWN
+            }
+            Result.Error(error)
+        }
+
+    } catch (e: IOException) {
+        Result.Error(DataError.Network.NO_INTERNET)
+    } catch (e: JsonParseException) {
+        Result.Error(DataError.Network.SERIALIZATION)
+    } catch (e: Exception) {
+        Result.Error(DataError.Network.UNKNOWN)
     }
 }
